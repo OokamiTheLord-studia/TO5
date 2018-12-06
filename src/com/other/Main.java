2165da7f9ce13c4b14de8fa9fa911bdca2f141ff
Jakub Markowski <kuba.xx5@outlook.com>
2018-12-06 15:36:59 +0100

    Dodałem todo

--- src/com/other/Main.java
+++ src/com/other/Main.java
@@ -9,6 +9,9 @@ public class Main {
         View v = new View();
 
         try {
+
+            //TODO: Dodać lowercase na wszystko
+            //TODO: Dodać validatory
             v.addPhoneNumber("Marcin Janusz Janowski", "567890");
         } catch (Exception e) {
             e.printStackTrace();
